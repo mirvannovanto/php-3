f510d0af3587bf9d6e5b8e2d095c6294ed270f5c
mirvan.novanto <muhammad_irfan_39@yahoo.co.id>
2020-12-17 02:48:54 +0700

    kode sudah benar!

--- ubah-huruf.php
+++ ubah-huruf.php
@@ -1,13 +1,24 @@
 <?php
 function ubah_huruf($string){
 //kode di sini
+$tampung = '';
+
+for($i = 0; $i < strlen($string); $i++){
+    $tampung .= chr(ord($string[$i])+1);
+}   
+    return $tampung;
+
 }
 
 // TEST CASES
 echo ubah_huruf('wow'); // xpx
+echo '<br>';
 echo ubah_huruf('developer'); // efwfmpqfs
+echo '<br>';
 echo ubah_huruf('laravel'); // mbsbwfm
+echo '<br>';
 echo ubah_huruf('keren'); // lfsfo
+echo '<br>';
 echo ubah_huruf('semangat'); // tfnbohbu
 
 ?>
